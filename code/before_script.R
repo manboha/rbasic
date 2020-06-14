52e641f1c3622afe682f9b01c170181e46eb8bd0
manboha <manboha@gmail.com>
2020-06-14 17:22:27 +0900

    update

---
+++ code/before_script.R
@@ -0,0 +1,11 @@
+
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  cache = TRUE, 
+  fig.align="center",
+  fig.pos="t"
+)
+set.seed(2016)
+options(digits = 3)
+options(dplyr.print_min = 4, dplyr.print_max = 4)
